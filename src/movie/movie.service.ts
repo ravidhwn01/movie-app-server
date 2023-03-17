0a7d4163dc6f47294ef76fa166ce67adc7a9a9a2
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-03-17 14:34:41 +0530

    some changes

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { Repository, Sequelize } from 'sequelize-typescript';
+import { MovieSchema } from 'src/schemas/movie.schema';
 
 @Injectable()
 export class MovieService {
+  private repository: Repository<MovieSchema>;
+  constructor(private sequelize: Sequelize) {
+    this.repository = this.sequelize.getRepository(MovieSchema);
+  }
   async getMovieData() {
-    const apiUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=f7105a03bb0a59160d88c3d60fbc2ff1&page=2`;
+    const apiUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=f7105a03bb0a59160d88c3d60fbc2ff1&page=1`;
 
     const response = await axios.get(apiUrl);
     // console.log(response.data);
+    // return this.repository.create(response.data);
     return response.data;
   }
 }
